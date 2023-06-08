95b3f5528be45c44a6c95393290eb081a65cf65f
Hugo Walandowitsch <hugowalando@gmail.com>
2023-06-08 12:08:21 +0200

    view-controller-routes

--- app/controllers/user_quizzes_controller.rb
+++ app/controllers/user_quizzes_controller.rb
@@ -2,4 +2,7 @@ class UserQuizzesController < ApplicationController
   def index
     @quizzes = UserQuiz.all
   end
+  def show
+    @quiz = UserQuiz.find(params[:id])
+  end
 end
--- app/models/user_world.rb
+++ app/models/user_world.rb
@@ -1,4 +1,5 @@
 class UserWorld < ApplicationRecord
   belongs_to :user
   belongs_to :world
+  has_many :user_quizzes
 end
