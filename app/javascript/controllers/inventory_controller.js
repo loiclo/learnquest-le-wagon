f2e8473dbcecee7a6f6ec39ae16090b147cef87d
Hugo Walandowitsch <hugowalando@gmail.com>
2023-06-15 12:15:01 +0200

    ne pas merge

--- app/javascript/controllers/inventory_controller.js
+++ app/javascript/controllers/inventory_controller.js
@@ -3,5 +3,9 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="inventory"
 export default class extends Controller {
   connect() {
+    console.log("controller connected")
+  }
+  avatarProcess(){
+    console.log(event.currentTarget)
   }
 }
